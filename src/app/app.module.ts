69e3d10323c8ecd994c41787f85dab5df98232bb
paras16jul1991 <paras16jul1991@gmail.com>
2019-09-02 15:24:21 +0530

    1 file modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { SheetComponentComponent } from './sheet-component/sheet-component.component';
-import { SheetComponent } from './sheet/sheet.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SheetComponentComponent, SheetComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
